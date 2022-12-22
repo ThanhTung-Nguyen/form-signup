9c15c470511d797fbbc37086ab50dd6c6e594835
Tung Nguyen <ntung.it312@gmail.com>
2022-12-22 16:16:04 +0700

    tạm thời hoàn thành CSS Responsive Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Card, Image, Layout } from "antd";
+import { Image, Layout } from "antd";
 import logo from "./images/logo-trang-01.png";
 import FormSignUp from "./components/FormSignUp";
 import "./App.css";
 const { Header, Content } = Layout;
-const {Meta} = Card;
 function App() {
   return (
     <Layout>
--- src/components/FormSignUp.tsx
+++ src/components/FormSignUp.tsx
@@ -6,7 +6,6 @@ import {
   Divider,
   Form,
   Input,
-  Radio,
   Select,
   Typography,
 } from "antd";
@@ -20,77 +19,85 @@ const dateFormatList = ["DD/MM/YYYY", "DD/MM/YY"];
 const { Title } = Typography;
 const FormSignUp = () => {
   return (
-    <div className="form">
-      <Form layout="vertical">
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-        >
-          <Avatar src={logo} />
-          <Title level={4}>ĐĂNG KÝ TƯ VẤN KẾT QUẢ KHÁM</Title>
-        </div>
+    <div className="form_cover">
+      <Form className="form" layout="vertical" colon={false} labelWrap={true} >
+        <Form.Item>
+          <Avatar src={logo} style={{ width: "4em", height: "4em" }} />
+          <Title level={3}>ĐĂNG KÝ TƯ VẤN KẾT QUẢ KHÁM</Title>
+        </Form.Item>
         <Divider />
-        <Form.Item label="Mã SID" required>
-          <Input placeholder="Nhập mã SID" />
-        </Form.Item>
-        <Form.Item label="Họ tên" required>
-          <Input placeholder="Nhập họ tên" />
-        </Form.Item>
-        <Form.Item label="Số điện thoại liên hệ" required>
-          <Input placeholder="Nhập số điện thoại" />
-        </Form.Item>
-        <Form.Item label="Ngày tư vấn" required>
-          <DatePicker
-            style={{ width: "100%" }}
-            // defaultValue={dayjs("", dateFormatList)}
-            format={dateFormatList}
-            placeholder="Nhập ngày hoặc chọn(vd: 27/11/1995....)"
+        <div className="change_layout">
+          <div>
+            <Form.Item htmlFor="sid" label="Mã SID" required>
+              <Input id="sid" placeholder="Nhập mã SID" allowClear />
+            </Form.Item>
+            <Form.Item htmlFor="name" label="Họ tên" required>
+              <Input id="name" placeholder="Nhập họ tên" allowClear />
+            </Form.Item>
+            <Form.Item htmlFor="phone" label="Số điện thoại liên hệ" required>
+              <Input id="phone" placeholder="Nhập số điện thoại" allowClear />
+            </Form.Item>
+          </div>
+          <Divider className="ke" />
+          <div>
+            <Form.Item htmlFor="date" label="Ngày tư vấn" required>
+              <DatePicker
+                id="date"
+                style={{ width: "100%" }}
+                // defaultValue={dayjs("", dateFormatList)}
+                format={dateFormatList}
+                placeholder="Nhập ngày hoặc chọn(vd: 27/11/1995....)"
+              />
+            </Form.Item>
+            <Form.Item htmlFor="time" label="Khung giờ tư vấn" required>
+              <Select
+                id="time"
+                style={{ width: "100%" }}
+                placeholder="Chọn"
+                allowClear
+                options={[
+                  {
+                    value: "1",
+                    label: "giờ thứ nhất",
+                  },
+                  {
+                    value: "2",
+                    label: "giờ thứ hai",
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item htmlFor="onoff" label="Hình thức tư vấn" required>
+              <Select
+                id="onoff"
+                style={{ width: "100%" }}
+                placeholder="Chọn"
+                allowClear
+                options={[
+                  {
+                    value: "off",
+                    label: "Tư vấn trực tiếp",
+                  },
+                  {
+                    value: "on",
+                    label: "Tư vấn online",
+                  },
+                ]}
+              />
+            </Form.Item>
+          </div>
+        </div>
+        <Form.Item htmlFor="cause" label="Lý do tư vấn">
+          <Input
+            id="cause"
+            placeholder="Nhập lý do"
+            style={{ height: "4.375em", borderRadius: "0.313em" }}
           />
-        </Form.Item>
-        <Form.Item label="Khung giờ tư vấn" required>
-          <Select
-            // defaultValue="1"
-            style={{ width: "100%" }}
-            placeholder="Chọn"
-            allowClear
-            options={[
-              {
-                value: "1",
-                label: "giờ thứ nhất",
-              },
-              {
-                value: "2",
-                label: "giờ thứ hai",
-              },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item label="Hình thức tư vấn" required>
-          <Select
-            // defaultValue="on"
-            style={{ width: "100%" }}
-            placeholder="Chọn"
-            allowClear
-            options={[
-              {
-                value: "off",
-                label: "Tư vấn trực tiếp",
-              },
-              {
-                value: "on",
-                label: "Tư vấn online",
-              },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item label="Lý do tư vấn">
-          <Input placeholder="Nhập lý do"/>
         </Form.Item>
         <Form.Item>
-          <Button style={{ width: "100%" }} type="primary">Đăng ký</Button>
+          <Button className="signup_btn" type="primary">
+            Đăng ký
+          </Button>
         </Form.Item>
       </Form>
     </div>
